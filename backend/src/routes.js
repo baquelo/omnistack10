8d225fc929e80a818d610df2b2e9ac238257df49
Marcelo <baquelo@gmail.com>
2020-01-14 12:26:09 -0300

    Init commit

---
+++ backend/src/routes.js
@@ -0,0 +1,27 @@
+const { Router } = require('express');
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
+
+const routes = Router();
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+
+// Query Params (GET): Ex.: http://localhost:3333/users?search=Marcelo
+// request.query (Filtros, ordenação, paginação, ...)
+
+// Route Params (PUT, DELETE): EX.: http://localhost:3333/users/1
+// request.params (Identificar um recurso na alteração ou remoção)
+
+// Body (POST, PUT):
+// request.body (Dados para criação ou alteração de um registro) 
+
+// MongoDB (Não-relacional)
+
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
+
+routes.get('/search', SearchController.index);
+
+module.exports = routes;
